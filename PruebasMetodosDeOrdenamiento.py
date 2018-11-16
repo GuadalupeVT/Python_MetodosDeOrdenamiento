807760d6e8ddd32c2c04d229272869a214be8a6a
Guadalupe Vazquez <guvadlt@gmail.com>
2018-11-16 16:52:18 -0600

    Se agrego la informacion de recorridos a las clases

--- PruebasMetodosDeOrdenamiento.py
+++ PruebasMetodosDeOrdenamiento.py
@@ -7,35 +7,55 @@ from time import time
 import random
 
 def ordenamientoPorSeleccion(numeros):
+    comparaciones=0
+    intercambios=0
+    recorridos=0
     start_time = time()
     for i in range(0,len(numeros)):    
         minimo = i
         for j in range(i+1,len(numeros)) :
+            comparaciones=comparaciones+1
             if numeros[j] < numeros[minimo] :
                 minimo = j
         if min is not i :
             aux = numeros[i]
             numeros[i] = numeros[minimo]
             numeros[minimo] = aux
+            intercambios=intercambios+1
+        recorridos=recorridos+1
     elapsed_time = time() - start_time
     print("Tiempo de ejecucion: %.10f seconds." % elapsed_time)
+    print ("Recoridos: "+str (recorridos))
+    print ("Intercambios: "+str (intercambios))
+    print ("Comparaciones: "+str (comparaciones))
     print numeros
+    
 
 def ordenamientoPorInsercion(numeros):
+    comparaciones=0
+    intercambios=0
+    recorridos=0
+    start_time = time()
     for i in range(1,len(numeros)):
         aux=numeros[i]
         j=i-1
+        comparaciones=comparaciones+1
         while j>=0:
             if(aux<numeros[j]):
                 numeros[j+1]=numeros[j]
-                
                 numeros[j]=aux
                 j=j-1
+                intercambios=intercambios+1
             else:
                 break
+            recorridos=recorridos+1
+    elapsed_time = time() - start_time
+    print("Tiempo de ejecucion: %.10f seconds." % elapsed_time)
+    print ("Recoridos: "+str (recorridos))
+    print ("Intercambios: "+str (intercambios))
+    print ("Comparaciones: "+str (comparaciones))
+    print numeros
     
-    
-
 arregloDesordenado1 = [0]  * 1000
 for i in range(1000):
     arregloDesordenado1[i] = random.randint(0, 100)
@@ -48,9 +68,6 @@ for i in range(100000):
 arregloDesordenado4 = [0]  * 1000000
 for i in range(1000000):
     arregloDesordenado4[i] = random.randint(0, 100)
+    
+ordenamientoPorInsercion(arregloDesordenado1)
 
-print "---------Ordenamiento por seleccion------------"          
-     
-edades=[54,26,93,17,77,31,44,55,20]
-print(arregloDesordenado1)
-ordenamientoPorSeleccion(arregloDesordenado1)
